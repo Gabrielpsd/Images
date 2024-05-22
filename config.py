2a826fa3fc3bd0abcf93d2e663de0f9e06a96471
Gabriel Dev <gabrielpsd3@gmail.com>
2024-05-22 16:53:33 -0400

    A new version of the program will be created, and the mode that the program works will change

---
+++ config.py
@@ -0,0 +1,13 @@
+from setuptools import setup 
+
+setup(
+    name="Image Search",
+    version="1.0.0",
+    description="""
+        This program searches a string passed by the user and brin images realated with 
+        the term.""",
+    author="Gabriel Dias",
+    libraries=
+    ["resquest",
+     "configparser"]
+)
